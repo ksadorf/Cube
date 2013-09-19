205c3c2d9d27a47f2b01012c2c23c1920bf5e642
ksad <noe.gaumont@gmail.com>
2013-09-19 02:10:03 +0200

    use localStorage to store highscore

--- www/LaBoite/gameScore.js
+++ www/LaBoite/gameScore.js
@@ -1,6 +1,7 @@
 ScoreElement = function(id,val){
 	this.dom=document.getElementById(id);
 	this.val=val;
+	this.dom.innerHTML=this.val;
 }
 
 ScoreElement.prototype={
@@ -17,9 +18,17 @@ ScoreElement.prototype={
 GameScore = function(divGame){
 	this.game = divGame;
 	this.curScore = new ScoreElement('score',0);
-	this.highScore = new ScoreElement('highScore',0);
+	
 	this.balleMorte = new ScoreElement('x',0);
 	this.balleSauve = new ScoreElement('y',0);
+	var oldHigh=localStorage.getItem("highScore");
+	
+	if(oldHigh){
+		this.highScore = new ScoreElement('highScore',oldHigh);
+	}else{
+		this.highScore = new ScoreElement('highScore',0);
+	}
+	
 	this.nbBalle = new ScoreElement('nbBalle',1);
 	
 	this.initHandler();
@@ -29,9 +38,11 @@ GameScore.prototype= {
 	updateScore : function(){
 		var score = this.balleSauve.val *2 - this.balleMorte.val;
 		this.curScore.maj(score);
-		if(this.curScore.val>= this.highScore.val){
+		if(this.curScore.val>= this.highScore.val){//Better highscore
 			this.curScore.dom.style.color="#339933";
 			this.highScore.maj(score);
+			localStorage.setItem("highScore",JSON.stringify(score));
+			
 		}else{
 			this.curScore.dom.style.color="";
 		}
